aaa7b3891da118c23e90073771768feacb9383ac
root19942 <42292743+root19942@users.noreply.github.com>
2020-01-27 08:15:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,33 @@ io.on('connection', function (socket) {
     var me = user
     io.sockets.emit('newuser',users)
   });
+  
+    socket.on('onWhriting', (user_to) => {
+      for (var i = users.length - 1; i >= 0; i--) {
+        if(users[i].user.id == user_to.id){
+          user.socket = socket.id; 
+          users[socket.id] = user; 
+          io.to(user[i].socket).emit('ImOnWhriting');
+          break
+        }
+      }
+
+    });
+
+    socket.on('onSendMessage', (user_to,message) => {
+      for (var i = users.length - 1; i >= 0; i--) {
+        if(users[i].id == user_to){ 
+          io.to(user[i].socket).emit('IHaveSendMessage',({body:message}));
+          break
+        }
+      }
+
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id]
+        io.sockets.emit('userDisconnected',users)
+    });
 });
 
 
